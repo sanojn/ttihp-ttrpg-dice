583e4a302dc1cbfed733bb62d8ee29bea790daa6
sanojn <58303304+sanojn@users.noreply.github.com>
2024-11-04 19:35:21 +0100

    Typo in top level module name

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_sanojn_ttrpg_dice_ihp (
+module tt_um_sanojn_ttrpgdice_ihp (
     input  wire [7:0] ui_in,    // buttons
     output wire [7:0] uo_out,   // 7segment signals
     input  wire [7:0] uio_in,   // [7:5] configures IO polarity
